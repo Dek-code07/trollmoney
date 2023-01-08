0c55adf22c33c5c3d64f1d66733d56b23319ff60
Cauê Souza de Lima <103838608+Dek-code07@users.noreply.github.com>
2023-01-08 12:13:48 -0300

    Add files via upload

---
+++ Troll/Script.js
@@ -0,0 +1,34 @@
+
+        function  Myfunction(botao){ 
+  
+          botao.style.position="absolute";
+          botao.style.left=Math.random()*500 +"px";
+          botao.style.top=Math.random()*500 +"px";
+          
+
+          
+
+  
+        }
+
+
+        function twofunction(){ 
+
+          text = document.getElementById("texto");
+
+          text.innerHTML="Que pena!"
+
+
+
+        }
+
+       botao.onclick=function(){ 
+
+        win = document.getElementById("texto2");
+
+        win.innerHTML="Também quero :)";
+
+       }
+
+  
+  
